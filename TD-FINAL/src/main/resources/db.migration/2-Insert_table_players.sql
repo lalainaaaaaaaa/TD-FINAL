195e1ab800b9f64391a39502be61c305992cf838
Ony Lalaina <randriamahefalalainaony@gmail.com>
2025-05-09 10:18:47 +0300

    fix: update insert_table_players related by table_players
     Update insert_table_players related by table_players

--- TD-FINAL/src/main/resources/db.migration/2-Insert_table_players.sql
+++ TD-FINAL/src/main/resources/db.migration/2-Insert_table_players.sql
@@ -1,5 +1,13 @@
-INSERT INTO players (name, number, position, nationality, age, teamId) VALUES
-                                                                          ('Vinicius Jr', 7, 'Attaquant', 'Brésil', 24, 1),          -- Real Madrid
-                                                                          ('Kylian Mbappé', 9, 'Attaquant', 'France', 26, 1),       -- Real Madrid
-                                                                          ('Lamine Yamal', 19, 'Attaquant', 'Espagne', 17, 2),      -- FC Barcelone
-                                                                          ('Ferran Torres', 7, 'Attaquant', 'Espagne', 25, 2);      -- FC Barcelone
+INSERT INTO Players (club_id, name, number, age, position, nationality) VALUES
+                                                                            (1, 'Goalkeeper 1', 1, 30, 'GOAL_KEEPER', 'Spanish'),
+                                                                            (1, 'Defender 1', 2, 25, 'DEFENSE', 'Spanish'),
+                                                                            (1, 'Midfielder 1', 5, 24, 'MIDFIELDER', 'Spanish'),
+                                                                            (1, 'Forward 1', 7, 17, 'STRIKER', 'Spanish'),
+                                                                            (2, 'Goalkeeper 2', 1, 21, 'GOAL_KEEPER', 'Spanish'),
+                                                                            (2, 'Defender 2', 2, 34, 'DEFENSE', 'Belgian'),
+                                                                            (2, 'Midfielder 2', 5, 29, 'MIDFIELDER', 'French'),
+                                                                            (2, 'Forward 2', 7, 18, 'STRIKER', 'German'),
+                                                                            (3, 'Goalkeeper 3', 1, 28, 'GOAL_KEEPER', 'Brazilian'),
+                                                                            (3, 'Defender 3', 2, 21, 'DEFENSE', 'Brazilian'),
+                                                                            (3, 'Midfielder 3', 5, 29, 'MIDFIELDER', 'French'),
+                                                                            (3, 'Forward 3', 7, 23, 'STRIKER', 'German');
