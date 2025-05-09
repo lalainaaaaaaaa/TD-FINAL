7301949ea5ae50d1cfc8ffbf039039c5da1f9bba
Ony Lalaina <randriamahefalalainaony@gmail.com>
2025-05-09 10:20:50 +0300

    fix: update insert_table_matches related by table_matches
    Update insert_table_matches related by table_matches

--- TD-FINAL/src/main/resources/db.migration/3-Insert_table_matches.sql
+++ TD-FINAL/src/main/resources/db.migration/3-Insert_table_matches.sql
@@ -1,5 +1,17 @@
-INSERT INTO matches(homeTeam, awayTeam, dateTime, stadium, season) VALUES
-                                                                      ('Real Madrid FC', 'FC Barcelone', '2025-05-01 21:00:00', 'Santiago Bernabeu', '2024-2025'),
-                                                                      ('FC Barcelone', 'Real Madrid FC', '2025-05-08 18:00:00', 'Lluís Companys', '2024-2025'),
-                                                                      ('Paris Saint Germain', 'Olympique de Marseille', '2025-05-01 21:00:00', 'Parc des princes', '2024-2025'),
-                                                                      ('Olympique de Marseille', 'Paris Saint Germain', '2025-05-08 18:00:00', 'Orange Vélodrome', '2024-2025');
+-- Season 2024
+INSERT INTO Matches (home_club_id, away_club_id, stadium, status, home_score, away_score) VALUES
+                                                                                              (1, 2, 'Stadium 1', 'NOT_STARTED', '(4) Forward 1 at 2''... Attacker 1 at 8'' Midfielder 1 at 50'' Defender 1 at 60''', '(1) Goalkeeper 1 at 1'' (ownGoal)'),
+                                                                                              (2, 3, 'Stadium 2', 'NOT_STARTED', '(1) Forward 2 at 88''', '(1) Forward 3 at 21'''),
+                                                                                              (1, 3, 'Stadium 1', 'NOT_STARTED', '(1) Forward 1 at 69''', 'Empty (0)'),
+                                                                                              (3, 2, 'Stadium 3', 'NOT_STARTED', 'Empty (0)', 'Empty (0)'),
+                                                                                              (2, 1, 'Stadium 2', 'NOT_STARTED', '(1) Forward 2 at 88''', '(0) Empty'),
+                                                                                              (3, 1, 'Stadium 3', 'NOT_STARTED', '(3) Goalkeeper 1 at 88''... Goalkeeper 1 at 89'' Goalkeeper 1 at 90''', '(2) Forward 1 at 56'' Midfielder 1 at 90''');
+
+-- Season 2025
+INSERT INTO Matches (home_club_id, away_club_id, stadium, status, home_score, away_score) VALUES
+                                                                                              (1, 2, 'Stadium 1', 'NOT_STARTED', '(4) Forward 1 at 2''... Attacker 1 at 8'' Midfielder 1 at 50'' Defender 1 at 60''', '(1) Goalkeeper 1 at 1'' (ownGoal)'),
+                                                                                              (2, 3, 'Stadium 2', 'NOT_STARTED', '(0) Empty', '(1) Forward 3 at 21'''),
+                                                                                              (1, 3, 'Stadium 1', 'NOT_STARTED', '(1) Forward 1 at 69''', 'Empty (0)'),
+                                                                                              (3, 2, 'Stadium 3', 'NOT_STARTED', 'Empty (0)', 'Empty (0)'),
+                                                                                              (2, 1, 'Stadium 2', 'NOT_STARTED', '(1) Forward 2 at 88''', '(0) Empty'),
+                                                                                              (3, 1, 'Stadium 3', 'NOT_STARTED', '(3) Goalkeeper 1 at 88''... Goalkeeper 1 at 89'' Goalkeeper 1 at 90''', '(2) Forward 1 at 56'' Midfielder 1 at 90''');
