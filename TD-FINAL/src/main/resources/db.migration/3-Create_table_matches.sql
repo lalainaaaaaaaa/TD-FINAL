91227e2558ef1e8aea2aa868031969d6879d6cf6
Ony Lalaina <randriamahefalalainaony@gmail.com>
2025-05-09 10:19:42 +0300

    fix: update table_matches realted by database
    Update table_matches realted by database

--- TD-FINAL/src/main/resources/db.migration/3-Create_table_matches.sql
+++ TD-FINAL/src/main/resources/db.migration/3-Create_table_matches.sql
@@ -1,8 +1,11 @@
-CREATE TABLE matches (
-                       id SERIAL PRIMARY KEY,
-                       homeTeam VARCHAR(100) NOT NULL,
-                       awayTeam VARCHAR(100) NOT NULL,
-                       dateTime TIMESTAMP,
-                       stadium VARCHAR(100),
-                       season VARCHAR(20)
+CREATE TABLE Matches (
+                         id SERIAL PRIMARY KEY,
+                         home_club_id INT,
+                         away_club_id INT,
+                         stadium VARCHAR(100),
+                         status VARCHAR(50),
+                         home_score VARCHAR(255),
+                         away_score VARCHAR(255),
+                         FOREIGN KEY (home_club_id) REFERENCES Clubs(id),
+                         FOREIGN KEY (away_club_id) REFERENCES Clubs(id)
 );
